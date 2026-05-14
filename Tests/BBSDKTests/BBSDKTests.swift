77a28ec059b467478ddf1a4341f994cc3cd143e7
Kovtun Vladimir <liamzik89@gmail.com>
2026-05-14 14:28:07 +0200

    hls sync, non zero offset

--- Tests/BBSDKTests/BBSDKTests.swift
+++ Tests/BBSDKTests/BBSDKTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import BBTVMediaKit
+@testable import BBSDK_SPM
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
